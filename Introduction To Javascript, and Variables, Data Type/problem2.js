d1c335fd84cce21c601b54e782836782052cd769
Mohd Zuber <119002523+saifizuber@users.noreply.github.com>
2024-04-14 21:47:34 +0530

    commit

---
+++ Introduction To Javascript, and Variables, Data Type/problem2.js
@@ -0,0 +1,8 @@
+var name = "Mohd Zuber"
+console.log(name)
+
+name = "Kayum Ali"
+console.log(name)
+
+name = "Roshan"
+console.log(name)
---
+++ Introduction To Javascript, and Variables, Data Type/problem3.js
@@ -0,0 +1,3 @@
+var name ="MOhd Zuber"
+var age = 21
+console.log(name,age)
